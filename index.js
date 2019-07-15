db9295c5b4f75c11a5e1d5f53060050003123ccf
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-15 07:53:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var takeANumber = (arr, value) => {
   const idx = arr.indexOf(value)
   if (idx === -1) {
     arr.push(value)
-    return `Welcome, ${value}. You are number ${arr.length} in line`
+    return `Welcome, ${value}. You are number ${arr.length} in line.`
   }
-  return `Welcome, ${value}. You are number ${idx} in line`
+  return `Welcome, ${value}. You are number ${idx} in line.`
 }
 
 var nowServing = (arr) => {
   if (arr.length === 0) return "There is nobody waiting to be served!"
-  return `Currently serving ${arr.pop()}`
+  return `Currently serving ${arr.pop()}.`
 }
 
 const currentLine = (arr) => {
